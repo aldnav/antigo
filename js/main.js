7cc276dbba2e42fc6703841d48a237bea3948efe
Aldrin Navarro <aldrinnavarro16@gmail.com>
2016-12-09 00:08:05 +0800

    Added variation of colors. Better repositioning of sprites. Added pause menu. Added kite.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var game = new Phaser.Game(720, 1280, Phaser.AUTO);
-
+var debug = false;
+var balloonColors = ['red', 'blue', 'green', 'orange', 'yellow'];
+var kiteColors = ['red', 'blue', 'green', 'orange', 'yellow'];
 
 var GameState = {
 
@@ -14,6 +16,10 @@ var GameState = {
     this.birdAngleDelta = 0.85;
     this.birdVelocityY = -1100;
     this.birdGravity = 2400;
+    this.balloonVelocityX = -300;
+    this.numberOfBalloons = 6;
+    this.kiteVelocityX = -400;
+    this.numberOfKites = 2;
   },
 
 
@@ -24,6 +30,7 @@ var GameState = {
 
     this.background = this.game.add.sprite(0, 0, 'background');
     this.balloons = this.game.add.group();
+    this.kites = this.game.add.group();
 
     this.bird = this.game.add.sprite(80, this.game.world.centerY / 2, 'bird');
     this.bird.anchor.setTo(0.5);
@@ -34,37 +41,63 @@ var GameState = {
     this.game.physics.arcade.enable(this.bird);
     this.bird.body.gravity.y = this.birdGravity;
     this.bird.body.collideWorldBounds = true;
-    this.bird.body.onCollide = new Phaser.Signal();
-    this.bird.body.onCollide.add(this.hitBalloon, this);
+    // this.bird.body.onCollide = new Phaser.Signal();
+    // this.bird.body.onCollide.add(this.hitBalloon, this);
+
+    for (var i = 0; i < this.numberOfBalloons; i++) {
+      this.addBalloon();
+    }
+    for (var i = 0; i < this.numberOfKites; i++) {
+      this.addKite();
+    }
+
+
+    pause_label = game.add.text(720 - 100, 20, 'Pause', { font: '24px Arial', fill: '#fff' });
+    pause_label.inputEnabled = true;
+
+    // this.pauseButton = this.game.add.sprite(0, 0, 'pauseButton');
+    this.pauseButton = pause_label;
+    this.pauseButton.inputEnabled = true;
+    this.pauseButton.events.onInputUp.add(function () {
+      this.game.paused = true;
+    },this);
+
+    this.game.input.onDown.add(function () {
+      if (this.game.paused) {
+        this.game.paused = false;
+      }
+    }, this);
 
     this.game.input.onDown.add(this.jump, this);
-
-    // this.addBalloon();
-    // this.addBalloon();
-    this.c = this.addBalloon();
   },
 
 
   update: function() {
     this.bird.angle += this.birdAngleDelta;
 
-    this.game.physics.arcade.collide(this.bird, this.balloons);
+    this.game.physics.arcade.collide(this.bird, this.balloons, this.hitBalloon, null, this);
+    this.game.physics.arcade.collide(this.bird, this.kites, this.restartGame);
 
-    // if (this.bird.y < 0 || this.bird.y > 1200)
-    //   this.restartGame();
+    if (this.bird.y < 60 || this.bird.y > 1200)
+      this.restartGame();
     var _gameState = this;
     this.balloons.forEach(function(balloon) {
       if (balloon.y <= (balloon.height/2.0) * -1 ||
           balloon.x <= (balloon.width/2.0) * -1) {
-        // balloon.kill();
         _gameState.killAndRevive(balloon);
+      }
+    });
+
+    this.kites.forEach(function(kite) {
+      if (kite.y <= (kite.height/2.0) * -1 ||
+          kite.x <= (kite.width/2.0) * -1) {
+        _gameState.killAndReviveKite(kite);
       }
     });
   },
 
 
   render: function() {
-    this.game.debug.spriteInfo(this.c, 64, 64);
   },
 
 
@@ -77,36 +110,80 @@ var GameState = {
   },
 
   hitBalloon: function(bird, balloon) {
-    // balloon.destroy();
     this.killAndRevive(balloon);
   },
 
   killAndRevive: function(balloon) {
+    balloon.animations.getAnimation('fly_balloon').destroy();
     balloon.kill();
     balloon.revive();
-    balloon.x = this.game.rnd.integerInRange(800, 1600);
-    balloon.y = this.game.rnd.integerInRange(60, 1240);
-    this.game.physics.arcade.accelerateToXY(balloon, 0, balloon.y);
+    balloon.x = (Math.floor(Math.random() * 8)*160 + this.game.width);
+    balloon.y = (Math.floor(Math.random() * 10)*110 + 110);
+    this.generateBalloonAnimation(balloon);
   },
 
 
   // balloon actions
   addBalloon: function(x, y, color) {
-      var x = x ? x: this.game.rnd.integerInRange(800, 1600);
-      var y = y ? y: this.game.rnd.integerInRange(60, 1240);
+      var x = x ? x: (Math.floor(Math.random() * 8)*160 + this.game.width);
+      var y = y ? y: (Math.floor(Math.random() * 10)*110 + 110);
       var balloon = this.game.add.sprite(x, y, 'sprite');
       balloon.scale.setTo(1.1);
-      balloon.animations.add(
-        'fly_balloon',
-        Phaser.Animation.generateFrameNames('balloon_red_', 1, 3, '', 2), 10, true);
-      balloon.animations.play('fly_balloon');
+      this.generateBalloonAnimation(balloon);
       this.balloons.add(balloon);
 
-      this.game.physics.arcade.enable(balloon, Phaser.Physics.ARCADE);
-      this.game.physics.arcade.accelerateToXY(balloon, 0, balloon.y, 180);
-      // balloon.body.gravity.y = -20;
-      // balloon.body.collideWorldBounds = true;
+      this.game.physics.arcade.enableBody(balloon);
+      balloon.body.allowGravity = false;
+      balloon.body.immovable = true;
+      balloon.body.velocity.x = this.balloonVelocityX;
       return balloon;
+  },
+
+  generateBalloonAnimation: function(balloon) {
+    balloon.color = balloonColors[Math.floor(Math.random() * balloonColors.length)];
+    var framePrefix = 'balloon_' + balloon.color + '_';
+    balloon.animations.add(
+      'fly_balloon',
+      Phaser.Animation.generateFrameNames(framePrefix, 1, 3, '', 2), 10, true);
+    balloon.animations.play('fly_balloon');
+  },
+
+  // kite actions
+  addKite: function(x, y, color) {
+    var x = x ? x: (Math.floor(Math.random() * 8)*160 + (this.game.width * 5));
+    var y = y ? y: (Math.floor(Math.random() * 10)*110 + 110);
+    var kite = this.game.add.sprite(x, y, 'sprite');
+    kite.scale.setTo(1.1);
+    this.generateKiteAnimation(kite);
+    this.kites.add(kite);
+
+    this.game.physics.arcade.enableBody(kite);
+    kite.body.allowGravity = false;
+    kite.body.immovable = true;
+    kite.body.velocity.x = this.kiteVelocityX;
+    return kite;
+  },
+
+  generateKiteAnimation: function(kite) {
+    kite.color = kiteColors[Math.floor(Math.random() * kiteColors.length)];
+    var framePrefix = 'kite_' + kite.color + '_';
+    kite.animations.add(
+      'fly_kite',
+      Phaser.Animation.generateFrameNames(framePrefix, 1, 3, '', 2), 10, true);
+    kite.animations.play('fly_kite');
+  },
+
+  killAndReviveKite: function(kite) {
+    kite.animations.getAnimation('fly_kite').destroy();
+    kite.kill();
+    kite.revive();
+    kite.x = (Math.floor(Math.random() * 8)*160 + (this.game.width * 3));
+    kite.y = (Math.floor(Math.random() * 10)*110 + 110);
+    this.generateKiteAnimation(kite);
+  },
+
+  hitKite: function(bird, kite) {
+    this.restartGame();
   },
 
 
